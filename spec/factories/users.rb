338494a8d4ae96f3a7ad41678df47195df79714c
fumtas1k <fumta01a7@gmail.com>
2022-03-09 15:52:29 +0900

    add:userモデル追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  name            :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    email { "MyString" }
+    password_digest { "MyString" }
+  end
+end
