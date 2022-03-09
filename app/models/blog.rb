54d75f23ab3b852be15e08af48384ebaa6a8d067
shouhei-yamamoto <elefish19920509@gmail.com>
2022-03-09 17:43:11 +0900

    userとblogの結び付け

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,6 +7,16 @@
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_blogs_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Blog < ApplicationRecord
+  belongs_to :user
 end
